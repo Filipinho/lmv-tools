1bac6e33ba36399944e6cdcec8b48db616830c6d
Molu Samuel <sgmolu@gmail.com>
2012-11-26 22:36:04 +0100

    Mise a jour module service

--- tools-service/src/main/java/com/lmv/tools/service/ToolsServiceManager.java
+++ tools-service/src/main/java/com/lmv/tools/service/ToolsServiceManager.java
@@ -1,11 +1,13 @@
 package com.lmv.tools.service;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * Hello world!
  * 
  */
 public class ToolsServiceManager {
@@ -19,16 +21,27 @@ public class ToolsServiceManager {
 	this.setSpringContext(new ClassPathXmlApplicationContext(springConfigFilePath));
     }
 
-    public static void main(String[] args) {
+   
+    /**
+     * 
+     * @return code execution du script
+     */
+    public static int execute(final File path) {
+	int exitCode = -100;
 	// Chargement du fichier de configuration
 	ToolsServiceManager tools = new ToolsServiceManager("tools-service-bean-config.xml");
+	final CmdLauncher launcher = (CmdLauncher) tools.getBean("cmdLauncher");
+	try {
+	    LOG.info( "Ressource from context : " + path );
+	    exitCode = launcher.execute(System.out, path);
+	} catch (IOException e) {
+	    LOG.error(e.getMessage(), e);
+	} catch (InterruptedException ie) {
+	    LOG.error(ie.getMessage(), ie);
+	}
 	
-	HelloService helloService = (HelloService)tools.getBean("helloService");
-	helloService.setName("Test");
-	helloService.printService();
-	LOG.info("log4j est initialise avec succes");
-	//Runtime.getRuntime().exec(cmdarray, envp, dir)
-
+	LOG.info("Code retour " + exitCode );
+	return exitCode;
     }
 
     public ToolsServiceManager() {
@@ -41,5 +54,9 @@ public class ToolsServiceManager {
 
     public void setSpringContext(ApplicationContext mSpringContext) {
 	this.mSpringContext = mSpringContext;
+    }
+
+    public ApplicationContext getSpringContext() {
+        return mSpringContext;
     }
 }
